0ecf9d0f9c885daa218b4aa356820a5ec9b62e86
AmirBI21 <102030136+AmirBI21@users.noreply.github.com>
2022-08-06 09:21:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 
 import streamlit
-streamlit.title('My parents new healthy dinner')
+streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 and Blueberry Omelet')
+streamlit.text('Kale, Spinach, and Rocket Smoothie')
+streamlit.text('Hard-boild Free-ranged Egg')
