5f0d29ae0b33311a9a9e3b4069a6721583d79701
AmirBI21 <102030136+AmirBI21@users.noreply.github.com>
2022-08-06 09:33:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 
 import streamlit
-streamlit.title('My Mom's New Healthy Dinner')
+streamlit.title("My Mom's New Healthy Dinner")
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega3 and Blueberry Omelet')
